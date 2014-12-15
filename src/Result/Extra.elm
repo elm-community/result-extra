1561763e278f2b05f458ed6be13eb329220150e7
Rehno Lindeque <errantkid@gmail.com>
2014-12-15 20:30:41 +0200

    Fix type signatures

--- src/Result/Extra.elm
+++ src/Result/Extra.elm
@@ -2,12 +2,12 @@ module Result.Extra where
 {-| Convenience functions for working with Result
 -}
 
-isOk : Result -> Bool
+isOk : Result a e -> Bool
 isOk x = case x of
           Ok  _ -> True
           Err _ -> False
 
-isErr : Result -> Bool
+isErr : Result a e -> Bool
 isErr x = case x of
             Ok  _ -> False
             Err _ -> True
