11ff0898894e30993f2eac271252f415fb9d1838
Chris <kqr@users.noreply.github.com>
2015-05-07 10:49:34 +0200

    Added the `flatten` convenience function...
    ...to do case analysis on a `Result` value without explicit pattern matching

--- src/Result/Extra.elm
+++ src/Result/Extra.elm
@@ -2,7 +2,7 @@ module Result.Extra where
 {-| Convenience functions for working with Result
 
 # Common Helpers
-@docs isOk, isErr
+@docs isOk, isErr, flatten
 
 -}
 
@@ -19,3 +19,11 @@ isErr : Result e a -> Bool
 isErr x = case x of
             Ok  _ -> False
             Err _ -> True
+
+{-| Turn a `Result e a` to an `a`, by applying the conversion
+function specified to the `e`.
+-}
+flatten : (e -> a) -> Result e a -> a
+flatten f x = case x of
+                Ok a -> a
+                Err e -> f e
