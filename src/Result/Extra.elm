31d3010364ffbdc138043202240df9ab0ddfe1a1
Richard Feldman <richard.t.feldman@gmail.com>
2015-08-04 14:31:55 -0700

    Add withDefault

--- src/Result/Extra.elm
+++ src/Result/Extra.elm
@@ -2,7 +2,7 @@ module Result.Extra where
 {-| Convenience functions for working with Result
 
 # Common Helpers
-@docs isOk, isErr, extract
+@docs isOk, isErr, extract, withDefault
 
 -}
 
@@ -27,3 +27,19 @@ extract : (e -> a) -> Result e a -> a
 extract f x = case x of
                 Ok a -> a
                 Err e -> f e
+
+
+{-| Returns a `Result`'s contents if the `Result` is an `Ok`,
+or the given default value if the `Result` is an `Err`.
+This is basically the same as running `Result.toMaybe` and then `Maybe.withDefault`.
+
+    0 == Result.withDefault 0 (String.toInt "this is not a valid integer!")
+-}
+withDefault : a -> Result x a -> a
+withDefault default result =
+    case result of
+        Ok value ->
+            value
+
+        Err _ ->
+            default
