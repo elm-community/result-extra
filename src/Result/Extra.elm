467ec6af7bc837e90b30d4310da19d63c34f6286
prozacchiwawa <art.yerkes@gmail.com>
2017-01-20 08:15:38 -0800

    Add merge which removes Result when the error type and the result type match

--- src/Result/Extra.elm
+++ src/Result/Extra.elm
@@ -11,6 +11,7 @@ module Result.Extra
         , orLazy
         , orElseLazy
         , orElse
+        , merge
         )
 
 {-| Convenience functions for working with `Result`.
@@ -181,3 +182,28 @@ orElse ra rb =
 
         Ok _ ->
             rb
+
+{-| Eliminate Result when error and success have been mapped to the same
+type, such as a message type.
+
+    merge (Ok 4)   == 4
+    merge (Err -1) == -1
+
+More pragmatically:
+
+    type Msg
+        = UserTypedInt Int
+        | UserInputError String
+
+    msgFromInput : String -> Msg
+    msgFromInput =
+        String.toInt
+        >> Result.mapError UserInputError
+        >> Result.map UserTypedInt
+        >> Result.Extra.merge
+-}
+merge : Result a a -> a
+merge r =
+    case r of
+        Ok rr -> rr
+        Err rr -> rr
