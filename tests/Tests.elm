2a31c3f54b7d73ed98d17fc17a859eda627c7716
Coury Ditch <cmditch@gmail.com>
2018-08-21 20:53:32 -0600

    Updated for 0.19

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -10,19 +10,19 @@ all =
     describe "Result.Extra Suite"
         [ describe "Basic Result.Extra tests"
             [ test "singleton" <|
-                \() ->
+                \_ ->
                     Expect.equal (singleton 42) (Ok 42)
             , test "andMap Err Err" <|
-                \() ->
+                \_ ->
                     Expect.equal (Err "Oh" |> andMap (Err "No!")) (Err "Oh")
             , test "andMap Err O" <|
-                \() ->
+                \_ ->
                     Expect.equal (Err "Oh" |> andMap (Ok 2)) (Err "Oh")
             , test "andMap Ok Err" <|
-                \() ->
+                \_ ->
                     Expect.equal (Ok ((+) 1) |> andMap (Err "No!")) (Err "No!")
             , test "andMap Ok Ok" <|
-                \() ->
+                \_ ->
                     Expect.equal (Ok ((+) 1) |> andMap (Ok 2)) (Ok 3)
             ]
         ]
