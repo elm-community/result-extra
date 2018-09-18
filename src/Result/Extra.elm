3664bc76bed4022f09930a0b74d8549cb5e06a94
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2018-09-18 16:04:31 +0200

    Add Combinators & Join
    Add a series of combineX functions for combining/mapping Results in
    lists and tuples.
    
    Add a join function for combining results of results with the same error
    type.

--- src/Result/Extra.elm
+++ src/Result/Extra.elm
@@ -1,5 +1,6 @@
 module Result.Extra exposing
-    ( isOk, isErr, extract, unwrap, unpack, error, mapBoth, combine, merge, partition, filter
+    ( isOk, isErr, extract, unwrap, unpack, error, mapBoth, combine, merge, join, partition, filter
+    , combineMap, combineFirst, combineSecond, combineBoth, combineMapFirst, combineMapSecond, combineMapBoth
     , singleton, andMap
     , or, orLazy, orElseLazy, orElse
     , toTask
@@ -10,7 +11,12 @@ module Result.Extra exposing
 
 # Common Helpers
 
-@docs isOk, isErr, extract, unwrap, unpack, error, mapBoth, combine, merge, partition, filter
+@docs isOk, isErr, extract, unwrap, unpack, error, mapBoth, combine, merge, join, partition, filter
+
+
+# Combining
+
+@docs combine, combineMap, combineFirst, combineSecond, combineBoth, combineMapFirst, combineMapSecond, combineMapBoth
 
 
 # Applying
@@ -129,11 +135,100 @@ mapBoth errFunc okFunc result =
             Err <| errFunc err
 
 
+
+-- Combining
+
+
 {-| Combine a list of results into a single result (holding a list).
+Also known as `sequence` on lists.
 -}
 combine : List (Result x a) -> Result x (List a)
 combine =
     List.foldr (Result.map2 (::)) (Ok [])
+
+
+{-| Map a function producing results on a list
+and combine those into a single result (holding a list).
+Also known as `traverse` on lists.
+
+    combineMap f xs == combine (List.map f xs)
+
+-}
+combineMap : (a -> Result x b) -> List a -> Result x (List b)
+combineMap f =
+    combine << List.map f
+
+
+{-| Pull a result out of the _first_ element of a tuple
+and combine it into a result holding the tuple's values.
+-}
+combineFirst : ( Result x a, c ) -> Result x ( a, c )
+combineFirst ( rx, y ) =
+    Result.map (\x -> Tuple.pair x y) rx
+
+
+{-| Pull a result out of the _second_ element of a tuple
+and combine it into a result holding the tuple's values.
+Also known as `sequence` on tuples.
+-}
+combineSecond : ( c, Result x a ) -> Result x ( c, a )
+combineSecond ( x, ry ) =
+    Result.map (Tuple.pair x) ry
+
+
+{-| Combine all results in a tuple
+into a single result holding the tuple's values.
+Also know as `bisequence` on tuples.
+-}
+combineBoth : ( Result x a, Result x b ) -> Result x ( a, b )
+combineBoth ( rx, ry ) =
+    Result.map2 Tuple.pair rx ry
+
+
+{-| Map a function producing results on the _first_ element of a tuple
+and then pull it out using `combineFirst`.
+Also know as `sequence` on tuples.
+
+    combineMapFirst f ( x, y )
+        == combineFirst (Tuple.mapFirst f ( x, y ))
+        == Result.map (flip Tuple.pair y) (f x)
+
+-}
+combineMapFirst : (a -> Result x b) -> ( a, c ) -> Result x ( b, c )
+combineMapFirst f =
+    combineFirst << Tuple.mapFirst f
+
+
+{-| Map a function producing results on the _second_ element of a tuple
+and then pull it out using `combineSecond`.
+Also know as `traverse` on tuples.
+
+    combineMapSecond f ( x, y )
+        == combineSecond (Tuple.mapSecond f ( x, y ))
+        == Result.map (Tuple.pair x) (f y)
+
+-}
+combineMapSecond : (a -> Result x b) -> ( c, a ) -> Result x ( c, b )
+combineMapSecond f =
+    combineSecond << Tuple.mapSecond f
+
+
+{-| Map a function producing results on the _both_ elements of a tuple
+and then pull them out using `combineBoth`.
+Also know as `bitraverse` on tuples.
+
+    combineMapBoth f g ( x, y )
+        == combineBoth (Tuple.mapBoth f g ( x, y ))
+        == Result.map2 Tuple.pair (f x) (g y)
+
+-}
+combineMapBoth : (a -> Result x c) -> (b -> Result x d) -> ( a, b ) -> Result x ( c, d )
+combineMapBoth f g =
+    combineBoth << Tuple.mapBoth f g
+
+
+
+-- Applying
 
 
 {-| Create a `singleton` from a value to an `Result` with a `Ok`
@@ -169,6 +264,10 @@ andMap ra rb =
 
         ( o, Ok fn ) ->
             Result.map fn o
+
+
+
+-- Alternatives
 
 
 {-| Like the Boolean `||` this will return the first value that is
@@ -244,6 +343,10 @@ orElse ra rb =
     or rb ra
 
 
+
+-- OTHER --
+
+
 {-| Eliminate Result when error and success have been mapped to the same
 type, such as a message type.
 
@@ -273,6 +376,28 @@ merge r =
 
         Err rr ->
             rr
+
+
+{-| Join contained results with the same error into one result.
+
+Usefull if you have a "result in a result":
+
+    join <| Ok (Ok 4) == Ok 4
+
+    join <| Ok (Err "message") == Err "message"
+
+-}
+join : Result x (Result x a) -> Result x a
+join r =
+    case r of
+        Err x ->
+            Err x
+
+        Ok (Err x) ->
+            Err x
+
+        Ok (Ok a) ->
+            Ok a
 
 
 {-| Partition a list of Results into two lists of values (successes
@@ -318,6 +443,10 @@ filter err predicate result =
 
         Err _ ->
             result
+
+
+
+-- Conversions
 
 
 {-| Convert a `Result` to a `Task` that will fail or succeed immediately.
