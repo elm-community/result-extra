2293ef6dd74d84cae7c5860455d28b2905840b43
Pavan Rikhi <pavan.rikhi@gmail.com>
2019-10-25 12:30:58 -0400

    Reformat Tests
    Break the tests into separate groups according to their documentation
    section.

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,47 +1,85 @@
-module Tests exposing (..)
+module Tests exposing (all)
 
 import Expect
-import Result.Extra exposing (..)
+import Result.Extra exposing (andMap, isOk, partition, singleton, toTask)
 import Task
-import Test exposing (..)
+import Test exposing (Test, describe, test)
 
 
 all : Test
 all =
-    describe "Result.Extra Suite"
-        [ describe "Basic Result.Extra tests"
-            [ test "singleton" <|
-                \_ ->
-                    Expect.equal (singleton 42) (Ok 42)
-            , test "andMap Err Err" <|
-                \_ ->
-                    Expect.equal (Err "Oh" |> andMap (Err "No!")) (Err "Oh")
-            , test "andMap Err O" <|
-                \_ ->
-                    Expect.equal (Err "Oh" |> andMap (Ok 2)) (Err "Oh")
-            , test "andMap Ok Err" <|
-                \_ ->
-                    Expect.equal (Ok ((+) 1) |> andMap (Err "No!")) (Err "No!")
-            , test "andMap Ok Ok" <|
-                \_ ->
-                    Expect.equal (Ok ((+) 1) |> andMap (Ok 2)) (Ok 3)
-            , test "partition []" <|
-                \_ ->
-                    Expect.equal (partition []) ( [], [] )
-            , test "partition [Ok]" <|
-                \_ ->
-                    Expect.equal (partition [ Ok 99 ]) ( [ 99 ], [] )
-            , test "partition [Err]" <|
-                \_ ->
-                    Expect.equal (partition [ Err 99 ]) ( [], [ 99 ] )
-            , test "partition [Ok, Err, Ok]" <|
-                \_ ->
-                    Expect.equal (partition [ Ok 99, Err "Nope", Ok -5 ]) ( [ 99, -5 ], [ "Nope" ] )
-            , test "toTask Ok" <|
-                \_ ->
-                    Expect.equal (toTask (Ok 4)) (Task.succeed 4)
-            , test "toTask Err" <|
-                \_ ->
-                    Expect.equal (toTask (Err "Oh")) (Task.fail "Oh")
-            ]
+    describe "Result.Extra"
+        [ commonHelperTests
+        , applyingTests
+        , toTaskTests
+        ]
+
+
+commonHelperTests : Test
+commonHelperTests =
+    describe "Common Helpers"
+        [ test "isOk - Ok" <|
+            \_ ->
+                Expect.true "Expected Ok value to return True" (isOk <| Ok 2)
+        , test "isOk - Err" <|
+            \_ -> Expect.false "Expected Err value to return False" (isOk <| Err 42)
+        , partitionTests
+        ]
+
+
+partitionTests : Test
+partitionTests =
+    describe "partition"
+        [ test "empty list" <|
+            \_ ->
+                Expect.equal (partition []) ( [], [] )
+        , test "only Ok" <|
+            \_ ->
+                Expect.equal (partition [ Ok 99 ]) ( [ 99 ], [] )
+        , test "only Err" <|
+            \_ ->
+                Expect.equal (partition [ Err 99 ]) ( [], [ 99 ] )
+        , test "mixed list" <|
+            \_ ->
+                Expect.equal (partition [ Ok 99, Err "Nope", Ok -5 ]) ( [ 99, -5 ], [ "Nope" ] )
+        ]
+
+
+applyingTests : Test
+applyingTests =
+    describe "Applying"
+        [ test "singleton" <|
+            \_ ->
+                Expect.equal (singleton 42) (Ok 42)
+        , andMapTests
+        ]
+
+
+andMapTests : Test
+andMapTests =
+    describe "andMap"
+        [ test "Err -> Err -> Err" <|
+            \_ ->
+                Expect.equal (Err "Oh" |> andMap (Err "No!")) (Err "Oh")
+        , test "Err -> Ok -> Err" <|
+            \_ ->
+                Expect.equal (Err "Oh" |> andMap (Ok 2)) (Err "Oh")
+        , test "Ok -> Err -> Err" <|
+            \_ ->
+                Expect.equal (Ok ((+) 1) |> andMap (Err "No!")) (Err "No!")
+        , test "Ok -> Ok -> Ok" <|
+            \_ ->
+                Expect.equal (Ok ((+) 1) |> andMap (Ok 2)) (Ok 3)
+        ]
+
+
+toTaskTests : Test
+toTaskTests =
+    describe "toTask"
+        [ test "Ok" <|
+            \_ ->
+                Expect.equal (toTask (Ok 4)) (Task.succeed 4)
+        , test "Err" <|
+            \_ ->
+                Expect.equal (toTask (Err "Oh")) (Task.fail "Oh")
         ]
